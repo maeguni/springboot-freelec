afbcef064b5d41f493fbb9ce1adcf8df1abff278
Haegun <maeguni@naver.com>
2020-06-29 18:24:32 +0900

    2020 - 06 - 29  commit

---
+++ src/main/java/com/example/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package com.example.book.springboot.service.posts;
+
+import com.example.book.springboot.domain.posts.PostsRepository;
+import com.example.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
---
+++ src/main/java/com/example/book/springboot/web/PostsApiController.java
@@ -0,0 +1,36 @@
+package com.example.book.springboot.web;
+
+
+import com.example.book.springboot.service.posts.PostsService;
+import com.example.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+
+        return postsService.save(requestDto);
+
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id,@RequestBody PostsSaveRequestDto requestDto){
+
+        return postsService.update(id,requestDto);
+
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsSaveRequestDto findById(@PathVariable Long id){
+
+        return postsService.findById(id);
+    }
+
+}
