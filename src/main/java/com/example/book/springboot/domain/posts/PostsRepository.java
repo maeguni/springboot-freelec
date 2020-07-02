5547a9a72c3dfa3d0ae17e8c7b5643c80e15291b
Haegun <maeguni@naver.com>
2020-07-02 17:59:28 +0900

    2020-07-02 게시판 등록화면 추가

--- src/main/java/com/example/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/example/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.example.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+    @Query("SELET p FROM Posts p ORFER BY p.id DESC")
+    List<Posts> findAllDesc();
 
 }
