d2955d7959ba0d55fbf681ab697d0d8f7f3a4261
Haegun <maeguni@naver.com>
2020-07-03 12:02:15 +0900

    2020-07-03 게시판 리스트 추가

--- src/main/java/com/example/book/springboot/web/IndexController.java
+++ src/main/java/com/example/book/springboot/web/IndexController.java
@@ -1,16 +1,20 @@
 package com.example.book.springboot.web;
 
+import com.example.book.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
 
