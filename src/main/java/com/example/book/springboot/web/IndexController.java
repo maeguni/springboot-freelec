4a5b05f87e21a664433431a92b343ba9e5e9847f
Haegun <maeguni@naver.com>
2020-07-07 16:53:25 +0900

    2020-07-07
    수정/삭제
    DDT 관련 숙지필요

--- src/main/java/com/example/book/springboot/web/IndexController.java
+++ src/main/java/com/example/book/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.example.book.springboot.web;
 
 import com.example.book.springboot.service.posts.PostsService;
+import com.example.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,5 +24,18 @@ public class IndexController {
     public String postsSave(){
         return "posts-save";
     }
+
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id,Model model){
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+
+        return "posts-update";
+
+    }
+
+
 
 }
