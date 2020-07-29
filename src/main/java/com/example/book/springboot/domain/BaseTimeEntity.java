347df10284fd6752a09ed3761a360180e1de363b
Haegun <maeguni@naver.com>
2020-07-29 10:42:05 +0900

    BaseTimeEntity 변경

--- src/main/java/com/example/book/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/example/book/springboot/domain/BaseTimeEntity.java
@@ -19,7 +19,7 @@ public class BaseTimeEntity {
 
     @CreatedDate
     // Entity가 생성되어 저장될때 시간이 자동 저장
-    private LocalDateTime createdDate;
+    private LocalDateTime creatDate;
 
     @LastModifiedDate
     //조회한 Entity의 값을 변경할 때 시간이 자동 저장
