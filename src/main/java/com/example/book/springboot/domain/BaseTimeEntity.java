ce5ad5094dcd40ce6c779819faf8a21cd7e6f7a5
Haegun <maeguni@naver.com>
2020-07-29 10:50:21 +0900

    .

--- src/main/java/com/example/book/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/example/book/springboot/domain/BaseTimeEntity.java
@@ -19,7 +19,7 @@ public class BaseTimeEntity {
 
     @CreatedDate
     // Entity가 생성되어 저장될때 시간이 자동 저장
-    private LocalDateTime creatDate;
+    private LocalDateTime createdDate;
 
     @LastModifiedDate
     //조회한 Entity의 값을 변경할 때 시간이 자동 저장
